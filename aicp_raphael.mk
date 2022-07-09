593188f7450da0ffef5cc81e944aeb26cb3c295e
diegocr@gmail.com <diegocr@gmail.com>
2022-07-09 17:53:12 +0200

    Aicpify
    Change-Id: Idf182cfac9b0109437b3181739c12f318b0b070f

--- aicp_raphael.mk
+++ aicp_raphael.mk
@@ -8,13 +8,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Inherit from raphael device
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-PRODUCT_NAME := lineage_raphael
+PRODUCT_NAME := aicp_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
